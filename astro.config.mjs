4285b5de533ce504c269eb64662df32d38d3574e
Damon Chu <dc3800@nyu.edu>
2023-08-14 23:09:12 -0400

    static version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config'
-import compress from 'astro-compress'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
+import mdx from '@astrojs/mdx';
 import lambdaAdapter from '@common-web/astro-lambda-edge';
+
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   server: {
     port: 3000,
     host: true
   },
   // site: 'https://dlib.nyu.edu/indianocean',
   // base: '/indianocean',
-  integrations: [compress(), mdx()],
-	adapter: lambdaAdapter()
-})
+  integrations: [mdx()]
+  // adapter: lambdaAdapter()
+});
