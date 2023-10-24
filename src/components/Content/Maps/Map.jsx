78ffa4552cb549cb33c3eeded79de25dd814f64b
Damon Chu <dc3800@nyu.edu>
2023-10-24 18:16:50 -0400

    Map updates

--- src/components/Content/Maps/Map.jsx
+++ src/components/Content/Maps/Map.jsx
@@ -1,12 +1,8 @@
 import {useState, useEffect} from 'react'
 
-import throttle from 'lodash.throttle';
-
 import MapPlaceholder from './MapPlaceholder';
 
 const Map = () => {
-
-	const [viewport,setViewport] = useState({width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0), height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0)})
 
 	const [isLoaded, setIsLoaded] = useState(false)
 
@@ -18,40 +14,26 @@ const Map = () => {
 		const body = document.querySelector('body')
 		const children = Array.from(body.children)
 		let height = document.documentElement.clientHeight
-		// console.log(`doc ${height}`)
 		for (let i = 0; i < children.length; i++){
 				height -= children[i].offsetHeight
 				if(height <= 0){
 					break;
 				}
 		}
-		// console.log(`newdoc ${height}`)
-		if (height > 0){
-			return height
+		const iframe = document.querySelector('iframe')
+		if(iframe){
+			iframe.style.height = `${height}px`
 		}
-		return 0
+		return height
 	}
 
 	window.addEventListener('resize', calculateAvailableHeight)
-
-const handleResize = throttle(()=>{
-	setViewport({width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0), height: window.innerHeight})
-	// console.log(viewport)
-},1500);
-
-
-useEffect(()=>{
-	// 	window.addEventListener('resize', handleResize)
-	// return()=>{
-	// 	window.removeEventListener('resize',handleResize)
-	// }
-},[viewport])
 
 	return (
 		<>
 			{!isLoaded && <MapPlaceholder height={calculateAvailableHeight()}/>}
 			<div className={!isLoaded?'mapContainerLoading':undefined}>
-			<iframe onLoad={mapLoad} style={{height:`${calculateAvailableHeight()}px`}} role="main" title="Viewer" className="widget book" id="book" name="book" allowFullScreen="" src={`https://sites.dlib.nyu.edu/viewer/maps/fales_io_map000002/1`}/>
+			<iframe onLoad={mapLoad} style={{height:calculateAvailableHeight()}} role="main" title="Viewer" className="widget book" id="book" name="book" allowFullScreen="" src={`https://sites.dlib.nyu.edu/viewer/maps/fales_io_map000002/1`}/>
 			</div>
 		</>
 	)
