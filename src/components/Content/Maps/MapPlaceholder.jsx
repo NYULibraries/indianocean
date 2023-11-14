7b3e4e5988476ccab47f7c495cb72824b0d2fb61
Damon Chu <dc3800@nyu.edu>
2023-11-14 10:58:35 -0500

    redirect changes

--- src/components/Content/Maps/MapPlaceholder.jsx
+++ src/components/Content/Maps/MapPlaceholder.jsx
@@ -4,7 +4,7 @@ import { Spin } from 'antd'
 
 const MapPlaceholder = (props) => {
 	const { height } = props
-	console.log(height)
+
 	return (
 		<div className='mapPlaceholder' style={{height:`${height}px`}}>
 			<Spin size='large' style={{'position':'relative', 'marginTop': '0%','marginLeft': '0%'}}/>
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -9,8 +9,14 @@
  export const pageNumber = url.searchParams.get('page') ? parseInt(url.searchParams.get('page'), 10) : 1
 
  // Get the last part of the URL
- export const id = url.searchParams.get('id') ? url.searchParams.get('id') : ''
+//  export const id = url.searchParams.get('id') ? url.searchParams.get('id') : ''
 
+ export const getID = (url)=>{
+	const parts = url.split('/')
+	return  parts[parts.length - 2] + '/' + parts[parts.length - 1]
+ }
+ export const parts = url.pathname.split('/')
+ export const id = parts[parts.length - 2] + '/' + parts[parts.length - 1]
 //  Get root of URL
  export const root = url.protocol + '//' + url.hostname + (url.port ? ':' + url.port : '');
 
