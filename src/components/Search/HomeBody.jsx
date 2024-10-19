f4619b82756711666c6704aa6d7bc545a133df21
Damon Chu <dc3800@nyu.edu>
2024-10-19 19:24:45 -0400

    Home page update

--- src/components/Search/HomeBody.jsx
+++ src/components/Search/HomeBody.jsx
@@ -1,56 +1,104 @@
 import { useState, useEffect, Suspense } from "react";
 import { useStore } from "@nanostores/react";
-import { changeSortStore, sort } from "../../stores/sortField";
-import { changePageNumStore, pageNum } from "../../stores/pageNum";
-import { changeSearchStore, search } from "../../stores/search";
+import { sort, changeSortStore, changeSortSubjectStore, sortedBySubject } from "../../stores/sortField";
+import { pageNum, changePageNumStore } from "../../stores/pageNum";
+import { search, changeSearchStore } from "../../stores/search";
 import { ConfigProvider } from "antd";
 import SearchResult from "./SearchResult";
 import SearchPagination from "./SearchTools/SearchPagination";
 import Unfound from "../Unfound";
 import theme from "../Styles/themeConfig";
-import { metatags } from "../../utils/Constants/metatags";
 import { env } from "../../utils/Constants/env";
-import { fetchIndex, fetchBrowse } from "../../utils/getDocuments";
+import { fetchBrowse, fetchIndex } from "../../utils/getDocuments";
+import { updateUrl } from "../../utils/url";
+import { metatags } from "../../utils/Constants/metatags";
 
 function HomeBody() {
-	const [data, setData] = useState([]);
+	const [data, setData] = useState([{}]);
 
 	const $sortField = useStore(sort);
-	const $pageNumField = useStore(pageNum);
 	const $searchField = useStore(search);
+	const $pageNumField = useStore(pageNum);
+	const $sortedBySubject = useStore(sortedBySubject);
 
 	const rows = env.PUBLIC_ROWS;
 
-	// Create an asynchronous function to fetch the data
-	const fetchData = async () => {
+	const initialFetch = async () => {
 		try {
 			const data = await fetchIndex();
 			setData(data);
 		} catch (error) {
-			// Handle errors here, e.g., display an error message or log the error
 			console.error("Error fetching data:", error);
 		}
 	};
-	const fetchData2 = async () => {
+
+	const fetchData = async () => {
 		try {
 			const data = await fetchBrowse($searchField, $pageNumField, $sortField);
 			setData(data);
 		} catch (error) {
-			// Handle errors here, e.g., display an error message or log the error
 			console.error("Error fetching data:", error);
 		}
 	};
 
 	useEffect(() => {
-		fetchData();
-		changeSearchStore("*:*");
-		changePageNumStore(1);
-		changeSortStore("default");
+		const storedSort = sessionStorage.getItem("sortField");
+		const storedSearch = sessionStorage.getItem("searchField");
+		const storedPage = parseInt(sessionStorage.getItem("pageNum"));
+
+		if (storedSort && storedSearch && !isNaN(storedPage)) {
+			changeSortStore(storedSort);
+			changeSearchStore(storedSearch);
+			changePageNumStore(storedPage);
+		}
+
+		// Run initialFetch if searchField is empty
+		if (!storedSearch) {
+			initialFetch();
+		}
+
+		const handlePopState = () => {
+			const urlParams = new URLSearchParams(window.location.search);
+			const search = decodeURIComponent(urlParams.get("q")) || "";
+			const page = parseInt(urlParams.get("page")) || 1;
+			const sortField = urlParams.get("sortField") || "";
+
+			changeSearchStore(search);
+			changePageNumStore(page);
+			changeSortStore(sortField);
+		};
+		window.addEventListener("popstate", handlePopState);
+
+		return () => {
+			window.removeEventListener("popstate", handlePopState);
+		};
 	}, []);
 
 	useEffect(() => {
-		fetchData2();
-	}, [$searchField, $pageNumField]);
+		sessionStorage.setItem("sortField", $sortField);
+		sessionStorage.setItem("searchField", $searchField);
+		sessionStorage.setItem("pageNum", $pageNumField);
+		
+		if ($searchField !== '*:*' && $searchField !== '' &&
+			$pageNumField !== undefined && $pageNumField !== '' &&
+			$sortField !== undefined && $sortField !== '') {
+			updateUrl($searchField, $pageNumField, $sortField);
+			fetchData();
+		} else {
+			initialFetch();
+		}
+		
+		changeSortSubjectStore(false);
+		const subjects = document.querySelectorAll(".md_subject");
+		subjects.forEach((subject) => {
+			if ($sortedBySubject) {
+				return;
+			}
+			if ($searchField == subject.textContent) {
+				changeSortSubjectStore(true);
+			}
+		});
+	}, [$searchField, $pageNumField, $sortField]);
 
 	return (
 		<Suspense fallback={<Unfound />}>
