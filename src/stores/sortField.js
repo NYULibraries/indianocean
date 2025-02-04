4f251ecf709e38e891240c3f0ec57cc0347c7711
Damon Chu <dc3800@nyu.edu>
2025-02-04 06:57:33 -0500

    replacestate instead of pushstate to stop repetitions in history stack

--- src/stores/sortField.js
+++ src/stores/sortField.js
@@ -1,18 +1,27 @@
 import { atom } from "nanostores";
 
-const url = new URL(window.location.href);
-const urlSortField = url.searchParams.get("sortField");
-const storedSort = sessionStorage.getItem("sortField");
+// Get initial state from URL on page load/refresh
+function getInitialSort() {
+	try {
+		const url = new URL(window.location.href);
+		return url.searchParams.get("sortField") || "default";
+	} catch {
+		return "default";
+	}
+}
 
-export const sort = atom(storedSort || (urlSortField ? urlSortField : "default"));
+export const sort = atom(getInitialSort());
+export const sortedBySubject = atom(true);
 
 export function changeSortStore(type) {
 	sort.set(type);
-	sessionStorage.setItem("sortField", type);
 }
-
-export const sortedBySubject = atom(true);
 
 export function changeSortSubjectStore(type) {
 	sortedBySubject.set(type);
+}
+
+export function resetSort() {
+	sort.set("default");
+	sortedBySubject.set(true);
 }
