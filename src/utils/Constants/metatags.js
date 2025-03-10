cc6e4414fde4c9ca9e011ed1fb33040d0e3d2261
Damon Chu <dc3800@nyu.edu>
2025-03-10 17:56:10 -0400

    Changed metatags for title and removed duplicate css imports at the bottom of layouts

--- src/utils/Constants/metatags.js
+++ src/utils/Constants/metatags.js
@@ -1,8 +1,7 @@
 export const metatags = {
 	siteName: "Indian Ocean Digital Collection",
 	pageTitle: "Monographs, Maps, Postcards, Prints",
-	bookTitle: "Indian Ocean Digital Collection - Book",
-	mapTitle: "Indian Ocean Digital Collection - Map",
+	searchTitle: "Indian Ocean Digital Collection - Browse",
 	defaultAuthor: "NYU Digital Library Technology Services",
 	poweredDesc: "Powered by NYU Digital Library Technology Services",
 	type: "website"
