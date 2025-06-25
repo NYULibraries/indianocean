bc0d82d7c69f46acfaa61865036f557086607518
Damon Chu <dc3800@nyu.edu>
2025-06-25 13:42:45 -0400

    about title

--- cypress/e2e/aboutPage.cy.js
+++ cypress/e2e/aboutPage.cy.js
@@ -1,17 +1,22 @@
-describe('About page visit', ()=>{
+describe('About page visit', () => {
 	describe('Text content load', () => {
-		it('Should load text content', ()=>{
+		it('Should load text content', () => {
 			cy.visit('/about');
-			cy.get('.page-title', {timeout: 3000}).contains('About')
-			cy.get('.maintext', {timeout: 3000}).find('h3').each($h3 => {
-			cy.wrap($h3).should('not.be.empty')})
-		})
-	 })
-	 describe('Check footer',()=>{
-		it('Should check footer is loaded',()=>{
-			cy.get('.footer-wrapper-top').children().each(($child)=>{
-				cy.wrap($child).should('exist')
-			})
-		})
-	 })
-})
+			cy.get('.page-title', { timeout: 3000 }).should('have.text', 'About');
+			cy.get('.maintext', { timeout: 3000 })
+				.find('h3')
+				.each(($h3) => {
+					cy.wrap($h3).should('not.be.empty');
+				});
+		});
+	});
+	describe('Check footer', () => {
+		it('Should check footer is loaded', () => {
+			cy.get('.footer-wrapper-top')
+				.children()
+				.each(($child) => {
+					cy.wrap($child).should('exist');
+				});
+		});
+	});
+});
